b322cf7a6e9c7fd5fe213f4cb7d70cc0052ade5a
Diego Souza <dee-souza@hotmail.com>
2019-08-19 11:28:53 -0300

    Feat: Install eslint and prettier and configure

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Learn More</Text>
+      <Text style={styles.welcome}>Read the docs to discover what:</Text>
+    </View>
+  );
+}
