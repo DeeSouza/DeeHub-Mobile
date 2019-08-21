811326167638fc3c3dc20bada18b1e45fbf1b82c
Diego Souza <dee-souza@hotmail.com>
2019-08-21 13:27:04 -0300

    Feat: Statusbar and install libs

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,16 @@
+import { NativeModules } from 'react-native';
+import Reactotron from 'reactotron-react-native';
+
+const host = NativeModules.SourceCode.scriptURL.split('://')[1].split(':')[0];
+
+if (__DEV__) {
+  const tron = Reactotron.configure({
+    host,
+  })
+    .useReactNative()
+    .connect();
+
+  console.tron = tron;
+
+  tron.clear();
+}
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159C1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
