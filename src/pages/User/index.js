c25644bf0101bcf79140617e3e0bdc2c90bd130e
Diego Souza <dee-souza@hotmail.com>
2019-08-22 16:48:05 -0300

    Feat: Feat: Pagination in the end reached of flatlist and refreshing

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 import api from '../../services/api';
 
 import {
@@ -23,45 +24,125 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    visible: false,
+    visibleList: false,
+    page: 0,
+    user: '',
+    refreshing: false,
   };
 
-  async componentDidMount() {
+  componentDidMount() {
     const { navigation } = this.props;
-    const user = navigation.getParam('user');
+    const params = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
-
-    this.setState({
-      stars: response.data,
+    this.setState({ user: params }, () => {
+      this.loadFavorites();
     });
   }
 
+  refreshList = () => {
+    this.setState(
+      {
+        page: 0,
+        refreshing: true,
+        visibleList: false,
+        stars: [],
+      },
+      () => this.loadFavorites()
+    );
+  };
+
+  loadFavorites = async () => {
+    const { page, stars, user } = this.state;
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page: page + 1 },
+    });
+
+    this.setState({ page: page + 1 });
+
+    if (response.data.length > 0) {
+      this.setState({
+        stars: [...stars, ...response.data],
+        visible: true,
+        visibleList: true,
+        refreshing: false,
+      });
+    }
+  };
+
   render() {
-    const { stars } = this.state;
+    const { stars, visible, visibleList, refreshing } = this.state;
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
     return (
       <Container>
         <Header>
-          <Avatar source={{ uri: user.avatar_url }} />
-          <Name>{user.name}</Name>
-          <Bio>{user.bio}</Bio>
+          <ShimmerPlaceHolder
+            autoRun
+            visible={visible}
+            height={100}
+            width={100}
+            reverse
+            style={{
+              backgroundColor: '#f5f5f5',
+              borderRadius: 50,
+              marginBottom: 10,
+            }}
+          >
+            <Avatar source={{ uri: user.avatar_url }} />
+          </ShimmerPlaceHolder>
+
+          <ShimmerPlaceHolder
+            autoRun
+            visible={visible}
+            style={{
+              backgroundColor: '#f5f5f5',
+            }}
+          >
+            <Name>{user.name}</Name>
+          </ShimmerPlaceHolder>
+
+          <ShimmerPlaceHolder
+            autoRun
+            visible={visible}
+            style={{
+              backgroundColor: '#f5f5f5',
+            }}
+          >
+            <Bio>{user.bio}</Bio>
+          </ShimmerPlaceHolder>
         </Header>
 
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        <ShimmerPlaceHolder
+          autoRun
+          visible={visibleList}
+          height={60}
+          style={{
+            backgroundColor: '#f5f5f5',
+            marginTop: 10,
+            width: '100%',
+          }}
+        >
+          <Stars
+            data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.1}
+            onEndReached={this.loadFavorites}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        </ShimmerPlaceHolder>
       </Container>
     );
   }
